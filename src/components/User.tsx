92f2bf2db92a2cd336c767e04a5b6d45e81eb256
kitpractice703 <kitpractice703@gmail.com>
2025-11-04 17:55:57 +0900

    need

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -1,9 +1,19 @@
-const user = () => {
-  return (
-    <>
-      <button onClick={() => alert('버튼을 클릭하셨습니다.')}>클릭</button>
-    </>
-  )
-}
+const User = ({
+  userObj,
+  clickHandler,
+}: {
+  userObj: { name: string; age: number }
+  clickHandler: () => void
+}) =>
+  // => 대한 의미 숙지하기
+  {
+    return (
+      <>
+        <p>이름: {userObj.name}</p>
+        <p>나이: {userObj.age}</p>
+        <button onClick={clickHandler}>클릭</button>
+      </>
+    )
+  }
 
-export default user
+export default User
